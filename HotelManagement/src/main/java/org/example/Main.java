917980620b3dc5c928d582a1bde60d6cfaef9e94
Berkintzl <berkintzlt@gmail.com>
2024-12-12 13:14:17 +0200

    database added
     mysql and project properties added

---
+++ HotelManagement/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import service.NotificationService;
+import utils.DatabaseConnection;
+import DAO.NotificationDAO;
+import DAO.ReservationDAO;
+
+import java.sql.Connection;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml")); 
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Hotel Management System Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            Connection connection = DatabaseConnection.getConnection();
+            NotificationService notificationService = new NotificationService(new NotificationDAO(connection), new ReservationDAO(connection));
+            NotificationService.NotificationScheduler scheduler = new NotificationService.NotificationScheduler(notificationService);
+            scheduler.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to load the FXML file. Check the file path.");
+        }
+    }
+}
