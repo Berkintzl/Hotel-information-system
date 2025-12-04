c40613c496a5ff170a111f60d03d42d17fb22fdc
Berkintzl <berkintzlt@gmail.com>
2025-12-04 13:16:00 +0200

    database

---
+++ HotelManagement/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO UserRole (id, role_name) VALUES (2, 'owner');
+INSERT IGNORE INTO UserRole (id, role_name) VALUES (3, 'manager');
+INSERT IGNORE INTO UserRole (id, role_name) VALUES (4, 'receptionist');
